43632bf19f3e5196202f34393f6ae8fed2b16ede
Dayou Zhang <zhan6350@umn.edu>
2023-03-09 21:39:25 -0600

    make molcasorbwriter symmetry aware

--- cclib/io/molcasorbwriter.py
+++ cclib/io/molcasorbwriter.py
@@ -12,6 +12,15 @@ import numpy
 from cclib.io import filewriter
 from cclib.parser import utils
 
+def generate_slices(nbas):
+    cumsum = numpy.cumsum([0, *nbas])
+    repeats = numpy.repeat(numpy.arange(len(nbas)), nbas)
+    return zip(repeats+1, cumsum[repeats], cumsum[repeats+1])
+
+def generate_mo_id(nbas):
+    for n in nbas:
+        yield from range(1, n+1)
+
 class MolcasOrb(filewriter.Writer):
     """A writer for OpenMolcas Orb files."""
 
@@ -30,6 +39,16 @@ class MolcasOrb(filewriter.Writer):
         data = self.ccdata
         nbasis = data.nbasis
         title = self.title
+        try:
+            nbas = data.metadata['nbas']
+            nsym = len(nbas)
+        except (AttributeError, KeyError):
+            nbas = [nbasis]
+            nsym = 1
+        nbas_str = ''.join('%8d'%i for i in nbas)
+        ao_splitter = numpy.cumsum(nbas)[:-1]
+        ao_slices = generate_slices(nbas)
+        mo_id = generate_mo_id(nbas)
         if title is None:
             title = 'Untitled Orbital'
         lines = []
@@ -37,31 +56,41 @@ class MolcasOrb(filewriter.Writer):
             lines.append(f'''#INPORB 2.2
 #INFO
 * {title}
-       0       1       0
-{nbasis:8d}
-{nbasis:8d}
+       0       {nsym}       0
+{nbas_str}
+{nbas_str}
 #ORB
 ''')
-            for n, i in enumerate(data.mocoeffs[0], 1):
-                lines.append(f"* ORBITAL    1{n:5d}\n ")
-                lines.append(numpy.array2string(i, max_line_width=115, formatter={'float_kind':lambda x: "%21.14E" % x})[1:-1])
+            for i in data.mocoeffs[0]:
+                s = next(ao_slices)
+                n = next(mo_id)
+                lines.append(f"* ORBITAL{s[0]:5d}{n:5d}\n ")
+                lines.append(numpy.array2string(i[s[1]:s[2]], max_line_width=115, formatter={'float_kind':lambda x: "%21.14E" % x})[1:-1])
                 lines.append('\n')
 
             try:
                 mo_occ = data.nooccnos
             except AttributeError:
                 mo_occ = numpy.zeros(nbasis)
-            lines.append('#OCC\n* OCCUPATION NUMBERS\n ')
-            lines.append(numpy.array2string(mo_occ, max_line_width=115, formatter={'float_kind':lambda x: "%21.14E" % x})[1:-1])
-            lines.append('\n')
+            lines.append('#OCC\n* OCCUPATION NUMBERS\n')
+            for i in numpy.split(mo_occ, ao_splitter):
+                if i.size == 0:
+                    continue
+                lines.append(' ')
+                lines.append(numpy.array2string(i, max_line_width=115, formatter={'float_kind':lambda x: "%21.14E" % x})[1:-1])
+                lines.append('\n')
 
             try:
                 mo_energy = data.moenergies[0]
             except AttributeError:
                 mo_energy = numpy.zeros(nbasis)
             mo_energy = utils.convertor(mo_energy, 'eV', 'hartree')
-            lines.append('#ONE\n* ONE ELECTRON ENERGIES\n ')
-            lines.append(numpy.array2string(mo_energy, max_line_width=122, formatter={'float_kind':lambda x: "%11.4E" % x})[1:-1])
-            lines.append('\n')
+            lines.append('#ONE\n* ONE ELECTRON ENERGIES\n')
+            for i in numpy.split(mo_energy, ao_splitter):
+                if i.size == 0:
+                    continue
+                lines.append(' ')
+                lines.append(numpy.array2string(i, max_line_width=122, formatter={'float_kind':lambda x: "%11.4E" % x})[1:-1])
+                lines.append('\n')
         return ''.join(lines)
 
